5fdbce0a16675a09beb371245540b747ecfec493
harsha.koneru@valuelabs.com <harsha.koneru@T03.ads.valuelabs.net>
2020-01-03 17:52:03 +0530

    Correction URLs

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -5,10 +5,10 @@ window._config = {
         clientId: 'bcgto49u0iskhm5vtme4am35h'
     },
     urls: {
-        portfolioURL: 'https://khv499.github.io/AWS/Cognito/app/index.html',
-        signInURL: 'https://khv499.github.io/AWS/Cognito/signIn.html',
-        signUpURL: 'https://khv499.github.io/AWS/Cognito/signUp.html',
-        forgotPasswordURL: 'https://khv499.github.io/AWS/Cognito/forgotPassword.html',
-        confirmForgotPasswordURL: 'https://khv499.github.io/AWS/Cognito/confirmForgotPassword.html'
+        portfolioURL: 'https://khv499.github.io/Sample-POC/Frontend/app/index.html',
+        signInURL: 'https://khv499.github.io/Sample-POC/Frontend/signIn.html',
+        signUpURL: 'https://khv499.github.io/Sample-POC/Frontend/signUp.html',
+        forgotPasswordURL: 'https://khv499.github.io/Sample-POC/Frontend/forgotPassword.html',
+        confirmForgotPasswordURL: 'https://khv499.github.io/Sample-POC/Frontend/onfirmForgotPassword.html'
     }   
 }
